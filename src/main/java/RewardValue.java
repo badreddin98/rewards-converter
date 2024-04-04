48ce07863fe2014786136c2088a13cf04925c356
badreddin98 <56277339+badreddin98@users.noreply.github.com>
2024-04-03 23:15:59 -0400

    Update RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,28 @@
 public class RewardValue {
     private final double cashValue;
-    private final double milesValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / 0.0035;
     }
 
     public RewardValue(int milesValue) {
-        this.milesValue = (double)milesValue;
-        this.cashValue = (double)milesValue * 0.0035;
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
-        return this.cashValue;
+        return cashValue;
     }
 
-    public double getMilesValue() {
-        return this.milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
